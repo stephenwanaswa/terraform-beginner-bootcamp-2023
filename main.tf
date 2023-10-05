9ec2adc5c262ad5acd32f70387270b042fc1dcb7
stephenwanaswa <stephenwanaswa@gmail.com>
2023-10-05 12:16:54 +0000

    #44 Test our Terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -25,28 +25,27 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "Text based games!"
   description = <<DESCRIPTION
 Text-based games, also known as text games or interactive fiction, are a genre of video games that rely primarily on text to convey the game's story, world, and interactions. These games have been around since the early days of computing and are characterized by their absence of graphics or visual elements, relying solely on text descriptions and user input
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdaq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
 
